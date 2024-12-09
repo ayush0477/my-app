35ba177870072c1b9517578a50b41d8f96ba60ee
ayush0477 <ayush0477@gmail.com>
2024-12-10 00:14:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={"d"}
         >
           Learn Reations
         </a>
