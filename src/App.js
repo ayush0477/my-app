7950c3f249e37f8c8463ccb4eaf73bf22f62a755
ayush0477 <ayush0477@gmail.com>
2025-03-13 01:33:51 +0530

    q

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-          onClick={"d"}
         >
           Learn Reations
         </a>
